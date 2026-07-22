7becc215911404390fecf18509d760ac8fa86ba7
Efe Oyali <efeoyali2@gmail.com>
2026-07-22 10:07:49 +0300

    updtaed board.c and control.c

--- charger_control-lib/control.c
+++ charger_control-lib/control.c
@@ -3,7 +3,9 @@ created by suBardagi
 */
 
 #include "control.h"
+#include "adc.h"
 #include "board-lib/board.h"
+#include "inc/hw_memmap.h"
 
 volatile uint16_t PFC_I_SENSE = 0;
 volatile uint16_t PFC_VDC_SENSE = 0;
@@ -14,6 +16,14 @@ volatile uint16_t DAB_I_OUT_SENSE = 0;
 
 __interrupt void adca1_isr(void)
 {
+    PFC_I_SENSE = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER0);
+    PFC_I_SENSE = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER1);
+    PFC_I_SENSE = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER2);
+
+    PFC_I_SENSE = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER3);
+    PFC_I_SENSE = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER4);
+    PFC_I_SENSE = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER5);
+
     ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
     Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP1);
 }
